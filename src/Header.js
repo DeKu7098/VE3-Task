5ee4b56ab19e831749b633d1f74a1b2535fcdda5
Arka <arkac720@gmail.com>
2022-11-12 21:30:30 +0530

    Search added

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 export const Header = () => {
-  
-    return (
-        <div>  
-            <Link to="/">     
-          <img src='https://www.freeiconspng.com/thumbs/house-png/home-house-silhouette-icon-building--public-domain-pictures--20.png' alt='home' width="6%"/>
-          </Link>
-        </div>
-      )
-  
-}
+  return (
+    <div>
+      <Link to="/">
+        <img
+          src="https://www.freeiconspng.com/thumbs/house-png/home-house-silhouette-icon-building--public-domain-pictures--20.png"
+          alt="home"
+          width="6%"
+        />
+      </Link>
+    </div>
+  );
+};
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,21 +1,58 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Header } from "./Header";
 import './home.css';
 import './Search.css';
 
+
 export const SearchPage = () => {
+  const [posts, setPosts] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+
+
+  useEffect(() => {
+    let arr=[]
+    const loadPosts = async () => {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setPosts(response.data);
+      for(let i=0;i<11;i++){
+        arr.push(response.data[i].name)
+      }
+      setPosts(arr)
+      console.log(response.data[1].name);
+      console.log(response.data.length)
+      
+    };
+
+    loadPosts();
+  }, []);
+
   return (
     <div className="containerSearch">
       <div className="home">
-        <Header />
+      <Header/>
       </div>
-      <div className="textcentre">
-        <h4>Type to search</h4>
-        <br />
-        <input type="text" placeholder="type text & press enter..." size="50" />
-        <br />
-        <p>Showing 0 results</p>
-      </div>
+      <h4>Type here to search....</h4>
+      <input
+        type="text"
+        placeholder="Type To Search..."
+        onChange={(e) => setSearchTitle(e.target.value)}
+      />
+      {posts
+          .filter((value) => {
+            if (searchTitle === "") {
+              return null;
+            } else if (
+              value.name.toLowerCase().includes(searchTitle.toLowerCase())
+            ) {
+              return value;
+            }
+          })
+          .map((item) => <h5 key={item.id}><>{item.name}</></h5>)
+      }
+      
     </div>
   );
-};
+}
